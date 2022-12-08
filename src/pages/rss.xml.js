9ce655555754051c568db49417b08d2c5880fea4
Chris Pennington <chris@codinginpublic.dev>
2022-12-07 23:44:04 -0700

    lesson-15

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,22 @@
+import rss from '@astrojs/rss';
+
+import { formatBlogPosts } from "../js/utils"
+
+const postImportResult = import.meta.glob('./blog/**/*.md', { eager: true });
+const posts = formatBlogPosts(Object.values(postImportResult));
+
+export const get = () => rss({
+  stylesheet: '/rss/styles.xsl',
+  title: 'My Astro Blog',
+  description: 'A humble Astronaut’s guide to the stars',
+  site: import.meta.env.SITE,
+  items: posts.map((post) => ({
+    link: post.url,
+    title: post.frontmatter.title,
+    pubDate: post.frontmatter.date,
+    description: post.frontmatter.description,
+    customData: `
+      <author>${post.frontmatter.author}</author>
+    `
+  }))
+});
