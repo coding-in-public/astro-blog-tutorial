ad0877f84f82832be34949e4068743613782fd91
Chris Pennington <chris@codinginpublic.dev>
2022-12-07 23:28:02 -0700

    lesson-14

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
+  site: 'https://astro-blog-cip.netlify.app',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  }), sitemap()]
 });
