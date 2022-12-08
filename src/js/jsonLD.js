37a42155eca9ca950dd4f208b6f110ad53055dec
Chris Pennington <chris@codinginpublic.dev>
2022-12-08 07:45:15 -0700

    lesson-16

---
+++ src/js/jsonLD.js
@@ -0,0 +1,35 @@
+import siteData from "../data/siteData.json"
+import { slugify } from "./utils";
+
+
+export default function jsonLDGenerator({ type, post, url }) {
+  if (type === 'post') {
+    return `<script type="application/ld+json">
+      {
+        "@context": "https://schema.org",
+        "@type": "BlogPosting",
+        "mainEntityOfPage": {
+          "@type": "WebPage",
+          "@id": "${url}"
+        },
+        "headline": "${post.title}",
+        "description": "${post.description}",
+        "image": "${post.image.src}",
+        "author": {
+          "@type": "Person",
+          "name": "${post.author}",
+          "url": "/author/${slugify(post.author)}"
+        },
+        "datePublished": "${post.date}"
+      }
+    </script>`;
+  }
+  return `<script type="application/ld+json">
+      {
+      "@context": "https://schema.org/",
+      "@type": "WebSite",
+      "name": "${siteData.title}",
+      "url": "${import.meta.env.SITE}"
+      }
+    </script>`;
+}
